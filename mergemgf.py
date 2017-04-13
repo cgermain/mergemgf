669694df20b8f74e9208e843baea2da40c64dada
Colin Germain <cgermain@gmail.com>
2017-04-13 00:02:34 -0400

    Cleaned up progress bar & comments.

--- mergemgf.py
+++ mergemgf.py
@@ -8,6 +8,8 @@ parser.add_argument('ms2_file', action="store", help="Path to the MS2 mgf file."
 parser.add_argument('ms3_file', action="store", help="Path to the MS3 mgf file.")
 
 def main():
+	#TODO run on a folder
+	#TODO pull in each mgf file & check for ms2/ms3
 	arguments = parser.parse_args()
 	if not os.path.isfile(arguments.ms2_file):
 		print "Please check that the path to the ms2 file is correct."
@@ -20,14 +22,15 @@ def main():
 	merge_spectrums(ms2_file, ms3_file)
 
 def merge_spectrums(ms2_file, ms3_file):
-	merged_mgf = []
-
 	current_count = 0
 	merged_count = 0
 	total_count = 0
 
+	#preloading the files into memory
+	#ms2 - so we have a total spectra count for the progress bar
+	#ms3 - so we don't have to read in repeatedly per ms2 spectra
 
-	#Only read from the ms3 file once & store in memory
+	merged_mgf = []
 	ms3_spectrum_list = []
 	ms2_spectrum_list = []
 
@@ -45,17 +48,18 @@ def merge_spectrums(ms2_file, ms3_file):
 	for ms2_spectrum in ms2_spectrum_list:
 		for ms3_spectrum in ms3_spectrum_list:
 			if compare_spectrums(ms2_spectrum, ms3_spectrum):
-				#print "MATCH FOUND!"
 				merged_xy = merge_xy_arrays(ms2_spectrum, ms3_spectrum)
-				#replace the arrays in ms2
+				#replace the arrays in the ms2 dictionary
 				ms2_spectrum['m/z array'] = merged_xy[0]
 				ms2_spectrum['intensity array'] = merged_xy[1]
 				merged_count += 1
+				#no need to continue searching after a match was found
 				break
-		merged_mgf.append(ms2_spectrum) #add if it was merged or not
+		merged_mgf.append(ms2_spectrum) #add no matter if it was merged or not
 		current_count += 1
-		progress_bar(current_count, total_count, "Merging")
+		write_progress_bar(current_count, total_count)
 
+	#TODO generate output filename & subdirectory
 	merged_mgf_filename = "combined_mgfs.mgf"
 
 	print "\nWriting merged MGF: " + merged_mgf_filename
@@ -91,14 +95,14 @@ def merge_xy_arrays(ms2_spectrum, ms3_spectrum):
 
 	return [merge_mz, merge_intensity] 
 
-def progress_bar(count, total, status=''):
-	bar_len = 60
-	filled_len = int(round(bar_len * count / float(total)))
+def write_progress_bar(current_count, total_count):
+	width = 60
+	complete_length = int(round(width * current_count / float(total_count)))
 
-	percents = round(100.0 * count / float(total), 1)
-	bar = '=' * filled_len + '-' * (bar_len - filled_len)
+	percent_complete = round(100.0 * current_count / float(total_count), 1)
+	bar = '=' * complete_length + '-' * (width - complete_length)
 
-	sys.stdout.write('%s: [%s] %s%s \r' % (status, bar, percents, '%'))
+	sys.stdout.write('Merging: [%s] %s%s \r' % (bar, percent_complete, '%'))
 	sys.stdout.flush()
 
 if __name__ == "__main__":
